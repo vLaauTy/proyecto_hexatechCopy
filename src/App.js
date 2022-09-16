3aeec387b2f0f995d0b6b464e92bc853c95d5a00
vLaauTy <96405301+vLaauTy@users.noreply.github.com>
2022-09-16 14:20:26 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@
 
 function App() {
   return (
-    <div className="App">
-      <h1>hola</h1>
-      <h2>Holaaaaaaaaaa 2</h2>
-      <h3>Holaaaaaaaaa 3</h3>
-    </div>
+    <>
+      
+    </>
   );
 }
 
